c8f26c3cc74bcf7a3fe78e6e976f3e4328bf1567
Stuti <stutitiwari102@gmail.com>
2025-05-26 18:41:20 +0530

    Initial commit

---
+++ src/main/java/com/stuti/Snipit/SnipitApplication.java
@@ -0,0 +1,13 @@
+package com.stuti.Snipit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stuti/Snipit/SnipitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuti.Snipit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
